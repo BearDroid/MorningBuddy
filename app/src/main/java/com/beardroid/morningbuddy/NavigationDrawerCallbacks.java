f676bf90c9e7af7704d4af4803acda1ddce55784
BearDroid <mossmm770@gmail.com>
2014-12-17 20:42:00 -0600

    First commit. Nav drawer base added.

---
+++ app/src/main/java/com/beardroid/morningbuddy/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package com.beardroid.morningbuddy;
+
+/**
+ * Created by Max on 12/17/2014.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
